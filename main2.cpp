9fb9504ed16b89b20c5b33c8e428144a5826667f
stud-15 <emilyafandieva123@gmail.com>
2024-10-24 11:59:39 +0400

    Initial commit

---
+++ main2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+class Logger {
+private:
+  inline static int counter = 0;
+  const int id;
+public:
+  Logger():id(++counter) {
+    std::cout<<"Logger():"<<id<<"\n";
+  }
+    Logger(const Logger& other): id(++counter){
+      std::cout<<"Logger(copy):"<<id<<"\n";
+    }
+    Logger& operator= (const Logger& other){
+      std::cout<<"Logger=:"<<id<<"\n";
+    }
+    ~Logger(){
+      --counter;
+      std::cout<<"~Logger()"<<id<<"\n";
+    }
+  };
